a02b7ec0852a75d09fd51cc939cdd44392cf6b42
karamalesa tv <karamalesa@gmail.com>
2020-10-01 17:30:26 +0300

    add HeroBlock

--- src/components/HeroBlock/HeroBlock.js
+++ src/components/HeroBlock/HeroBlock.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import './heroblock.scss';
+import './HeroBlock.scss';
 
-export default function HeroBlock({title, link, logos}) {
-  const logosImg = logos.map((item) => <img src={item} alt="partner-logo" className="hero-partners-item"/>);
-  const navlinks = link.map((linkitem) => <a href={linkitem.href} className="hero-navigation-item">{linkitem.title}</a>)
-  
+const HeroBlock = ({title, links, logos}) => {
+	const logosImages = logos.map((logoItem) => <img src={logoItem} alt="partner-logo" className="hero-partners-item"/>);
+	const navItems = links.map((link) => <a href={link.href} className="hero-navigation-item">{link.title}</a>);
+	
+	return (
+		<section className="hero">
+			<div className="container">
+				<div className="hero-partners">
+					{logosImages}
+				</div>
+				<nav className="hero-navigation">
+					{navItems}
+				</nav>
+				<h1 className="hero-title">{title}</h1>
+				<a href="#" className="hero-link">Отследить</a>
+			</div>
+		</section>
+	);
+};
 
-  return (
-    <section className="hero">
-      
-      <div className="container">
-        <div className="hero-partners">
-            {logosImg}
-        </div>
-        
-        <div className="hero-pagination">
-          {navlinks}
-        </div>
-        <h1 className="hero-title">{title}</h1>
-        <a href="#button" className="hero-link">Отследить</a>
-
-      </div>
-    </section>
-    
-  )
-}
-
+export default HeroBlock;
