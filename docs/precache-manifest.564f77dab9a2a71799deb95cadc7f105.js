3fd7e1cbd385f33cac88fc1b1424f30595508dfb
karamalesa tv <karamalesa@gmail.com>
2020-10-01 18:02:12 +0300

    create "docs"

---
+++ docs/precache-manifest.564f77dab9a2a71799deb95cadc7f105.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0c9fcfd7cc20b6ee48d2984002c3e9",
+    "url": "/react-landing-page/index.html"
+  },
+  {
+    "revision": "aa566de41c325cb81925",
+    "url": "/react-landing-page/static/css/2.8936a6e9.chunk.css"
+  },
+  {
+    "revision": "d0de5650dc5c0d6171ef",
+    "url": "/react-landing-page/static/css/main.e1168af9.chunk.css"
+  },
+  {
+    "revision": "aa566de41c325cb81925",
+    "url": "/react-landing-page/static/js/2.8e061a87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-landing-page/static/js/2.8e061a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0de5650dc5c0d6171ef",
+    "url": "/react-landing-page/static/js/main.9ebbd6e3.chunk.js"
+  },
+  {
+    "revision": "44bc5688745e393929c6",
+    "url": "/react-landing-page/static/js/runtime-main.a40f56c1.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/react-landing-page/static/media/GC_logo.0662d9de.svg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/react-landing-page/static/media/ITN_logo.2f311a05.svg"
+  },
+  {
+    "revision": "43f9dac62ffb9209e823b91b7504c5ec",
+    "url": "/react-landing-page/static/media/energetics.43f9dac6.jpg"
+  },
+  {
+    "revision": "fb084e1e416e340515649cb48eb04f6e",
+    "url": "/react-landing-page/static/media/energy.fb084e1e.jpg"
+  },
+  {
+    "revision": "5d86912d3747f4d2fb2419bd7d2de74b",
+    "url": "/react-landing-page/static/media/hero-bg.5d86912d.jpg"
+  },
+  {
+    "revision": "5ec44869469e2f3c9db3551c44662f88",
+    "url": "/react-landing-page/static/media/industry.5ec44869.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/react-landing-page/static/media/shid_emozhl_logo.341a487c.svg"
+  },
+  {
+    "revision": "f8f67844654cd9fc78dd57834ecfed6c",
+    "url": "/react-landing-page/static/media/sity_planing.f8f67844.jpg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/react-landing-page/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/react-landing-page/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/react-landing-page/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/react-landing-page/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/react-landing-page/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "65643b05446e10934c8778999860e9de",
+    "url": "/react-landing-page/static/media/transport.65643b05.jpg"
+  },
+  {
+    "revision": "a8b6caeec11d336276f5ed05a63fc938",
+    "url": "/react-landing-page/static/media/utilisation.a8b6caee.jpg"
+  }
+]);
