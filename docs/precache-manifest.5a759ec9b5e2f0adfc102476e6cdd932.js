d770c454279c1315c101ad482fb15675e085aee0
karamalesa tv <karamalesa@gmail.com>
2020-10-04 21:13:46 +0300

    new docs

---
+++ docs/precache-manifest.5a759ec9b5e2f0adfc102476e6cdd932.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667c7dd366f010bd999034847ac82981",
+    "url": "/air-quality/index.html"
+  },
+  {
+    "revision": "e9b6851547426b7f1dbd",
+    "url": "/air-quality/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "c68014c14fb6d595d524",
+    "url": "/air-quality/static/css/main.4c590b1f.chunk.css"
+  },
+  {
+    "revision": "e9b6851547426b7f1dbd",
+    "url": "/air-quality/static/js/2.195fc9c7.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/air-quality/static/js/2.195fc9c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68014c14fb6d595d524",
+    "url": "/air-quality/static/js/main.278565a9.chunk.js"
+  },
+  {
+    "revision": "036305f7332184465a41",
+    "url": "/air-quality/static/js/runtime-main.548f3858.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/air-quality/static/media/GC_logo.0662d9de.svg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/air-quality/static/media/ITN_logo.2f311a05.svg"
+  },
+  {
+    "revision": "08095dbe3008c73fc536c97da496d5b3",
+    "url": "/air-quality/static/media/energetics.08095dbe.jpg"
+  },
+  {
+    "revision": "a4578f1738b9f56b3fe5af6656759f6e",
+    "url": "/air-quality/static/media/energy.a4578f17.jpg"
+  },
+  {
+    "revision": "5d86912d3747f4d2fb2419bd7d2de74b",
+    "url": "/air-quality/static/media/hero-bg.5d86912d.jpg"
+  },
+  {
+    "revision": "6183a899912424d4fcd7d8164bff521c",
+    "url": "/air-quality/static/media/industry.6183a899.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/air-quality/static/media/shid_emozhl_logo.341a487c.svg"
+  },
+  {
+    "revision": "a4f39f198517f02bcb094ad79024e5b9",
+    "url": "/air-quality/static/media/sity_planing.a4f39f19.jpg"
+  },
+  {
+    "revision": "95abad8c822d43e5d5a2f67833508932",
+    "url": "/air-quality/static/media/slide-1.95abad8c.jpg"
+  },
+  {
+    "revision": "07c421fbfe370441d9769df55ba1efdf",
+    "url": "/air-quality/static/media/slide-2.07c421fb.jpg"
+  },
+  {
+    "revision": "5b39313319d47f5f27fc7061cd73126f",
+    "url": "/air-quality/static/media/slide-3.5b393133.jpg"
+  },
+  {
+    "revision": "af3ae65ebff2a5febda54efa9adeb4e4",
+    "url": "/air-quality/static/media/slide-4.af3ae65e.jpg"
+  },
+  {
+    "revision": "4e2e07fad394e1f35e9efbf0478d973e",
+    "url": "/air-quality/static/media/slide-5.4e2e07fa.jpg"
+  },
+  {
+    "revision": "8c89b45c7eb7f9e0d60878c36b4962dc",
+    "url": "/air-quality/static/media/transport.8c89b45c.jpg"
+  },
+  {
+    "revision": "205e668a12fc5c4eb70fd5988fb8ea9a",
+    "url": "/air-quality/static/media/utilisation.205e668a.jpg"
+  }
+]);
